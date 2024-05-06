d690182cdefab95a5f3b96a1cff91c55a6b36544
FAKUBA08 <badmusfaruq01@gmail.com>
2024-05-06 15:51:36 +0100

    all

---
+++ UU/style.js
@@ -0,0 +1,159 @@
+let home=document.querySelector('.home')
+  let abort=document.querySelector('.abort')
+  let contactInner=document.querySelector('.contactInner')
+  let number=document.querySelector('.number')
+  let press=document.querySelector('.press')
+ let buttons=document.querySelectorAll('.seen')
+ let display=document.getElementById('display')
+ let contact=document.querySelector('.contact')
+ let mobile=document.querySelector('.mobile')
+ let contactBtn=document.getElementById('contactBtn')
+ let contactList=document.querySelector('.contactList')
+ let announcement=document.querySelector('.announcement')
+ let toggles = document.querySelector(".toggles");
+ let home2=document.getElementById('home2')
+ let call =document.querySelector('.call')
+
+ buttons.forEach(function(el,i){
+  el.addEventListener('click',function(o){
+  display.innerHTML+=`${i+1}`
+  })
+ })
+ function check(numbers){
+   display.innerHTML+=numbers
+ }
+number.addEventListener('click',function(){
+  document.querySelector('.press').style.display='block'
+  document.querySelector('.number').style.display='none'
+  document.getElementById('display').style.display='block'
+  
+ })
+ abort.addEventListener('click',function(){
+  document.querySelector('.press').style.display='none'
+  document.querySelector('.number').style.display='grid'
+  document.getElementById('display').style.display='none'
+  // alert('kjhgfd')
+  
+ })
+ home.addEventListener('click',function(){
+  window.location.href='/index.html'
+
+ })
+ contactBtn.addEventListener('click',function(){
+ document.querySelector(".contactList").style.display='block'
+ document.querySelector('.press').style.display='none'
+ document.getElementById('display').style.display='none'
+ document.querySelector('.contactInner').style.backgroundColor=' rgba(0, 0, 0, 0.603)'
+ document.querySelector('.number').style.display='none'
+//  alert('lkj')
+ 
+ })
+//  let arr=[]
+ let del=document.getElementById('delete')
+ let box1=document.getElementById('box1')
+ let box2=document.getElementById('box2')
+ let box3=document.getElementById('box3')
+ let save=document.querySelector('.save')
+ let container=document.querySelector(".container")
+ let lContainer=document.querySelector(".lContainer")
+ let contactSave=document.querySelector('.contactSave')
+ let accordion=document.getElementsByClassName('accordion')
+ let acc=document.getElementsByClassName("accordion")
+ let sims=document.querySelector('.sims')
+ let cancelCall=document.querySelector('.cancelCall')
+ let store=''
+let dataIn= localStorage.getItem('store') ? JSON.parse(localStorage.getItem('store')) : [];
+seen(dataIn);
+ save.addEventListener('click',function(){
+
+  let details={name:box1.value,lname:box2.value,phone:box3.value}
+ if (box1.value==='' && box2.value==='' && box3.value==='') {
+    box1.value=''
+    box2.value=''
+    box3.value=''
+    alert('field cannot be empty')
+  }else{
+    dataIn.push(details)
+    box1.value=''
+    box2.value=''
+    box3.value=''
+    console.log(dataIn);
+    alert('contact Saved')
+    localStorage.setItem('store', JSON.stringify(dataIn));
+    dataIn = JSON.parse(localStorage.getItem('store'));
+    seen(dataIn);
+
+  }
+  document.querySelector(".contactSave").style.display='none'
+  // console.log(arr);
+ })
+ function seen(arr){
+  container.innerHTML=''
+  arr.forEach(function(el,i){
+    container.innerHTML+=`<h4>${el.name} ${' '}${el.lname}${' '}  <br> ${el.phone} <button   onclick="remove(${i})"   id="delete" ><img src="pic/folder_trash__1_-removebg-preview.png" alt=""></button></br>
+</h4>`
+
+   })
+   console.log(container);
+}
+function remove(index){
+  dataIn.splice(index,1)
+  // container.innerHTML+=''
+  localStorage.setItem('store', JSON.stringify(dataIn));
+  dataIn = JSON.parse(localStorage.getItem('store'));
+  seen(dataIn);
+}
+
+
+
+for (let index = 0; index < accordion.length; index++) {
+  accordion[index].addEventListener('click',function(){
+   let seen=this.nextElementSibling;
+   if (seen.style.display==='block') {
+    seen.style.display='none'
+    document.querySelector(".contactSave").style.display='none'
+   }else{
+    seen.style.display='block'
+    document.querySelector(".contactSave").style.display='block'
+   }
+  })
+  
+}
+toggles.addEventListener('click',function(){
+  window.location.href='/index.html'
+
+})
+home2.addEventListener('click',function(){
+  document.querySelector(".contactList").style.display='none'
+  document.querySelector('.press').style.display='block'
+  document.getElementById('display').style.display='block'
+  document.querySelector('.contactInner').style.backgroundColor= 'rgba(0, 0, 0, 0.898)'
+  document.querySelector('.number').style.display='none'
+
+})
+function bSpace(){
+ display.innerHTML=display.innerHTML.slice(0,-1)
+}
+container.addEventListener('dblclick',function(){
+  document.querySelector(".contactList").style.display='none'
+  document.querySelector('.press').style.display='none'
+  document.getElementById('display').style.display='block'
+  document.querySelector('.contactInner').style.backgroundColor= 'rgba(0, 0, 0, 0.898)'
+  document.querySelector('.number').style.display='none'
+  document.querySelector('.sims').style.display='block'
+  document.getElementById('display').style.display='none'
+
+})
+cancelCall.addEventListener('click',function(){
+  document.querySelector('.sims').style.display='none'
+  document.querySelector('.press').style.display='block'
+  document.getElementById('display').style.display='block'
+})
+call.addEventListener('click',function(){
+  document.querySelector('.sims').style.display='block'
+  document.querySelector('.press').style.display='none'
+  document.getElementById('display').style.display='none'
+})
+
+
+
---
+++ index.js
@@ -0,0 +1,192 @@
+let toggles = document.getElementsByClassName("toggles");
+let mobileBox = document.querySelector('.mobile');
+let page = document.getElementById('page');
+let time = document.querySelector(".time");
+let monthTime = document.querySelector(".monthTime");
+let On = document.getElementById('On');
+let menuAdd = document.querySelector('.menuAdd');
+let phone = document.querySelector('.phone');
+let canSave = false;
+let menuBtn = document.querySelector('.menuBtn');
+let otherMenu=document.querySelector('.otherMenu')
+let gallary=document.querySelector('.gallary')
+let camera=document.querySelector('.camera')
+let cameraShot=document.querySelector('.cameraShot')
+let capture=document.getElementById('capture')
+let home=document.querySelector('.home')
+
+function saveToggleState(index, state) {
+    localStorage.setItem(`toggleState${index}`, state);
+}
+
+function loadToggleState(index) {
+    return localStorage.getItem(`toggleState${index}`);
+}
+
+function updatePageState() {
+    let allOn = true;
+    for (let index = 0; index < toggles.length; index++) {
+        let panel = toggles[index].nextElementSibling;
+        let savedState = loadToggleState(index);
+        if (savedState === 'block') {
+            panel.style.display = 'block';
+            allOn = false; 
+        } else {
+            panel.style.display = 'none';
+        }
+    }
+    if (allOn) {
+        mobileBox.classList.remove('homeScreen');
+        mobileBox.classList.add('phoneInner');
+        document.getElementById('page').style.display = 'none';
+        document.querySelector('.time').style.display = 'none';
+        document.querySelector('.monthTime').style.display = 'none';
+        mobileBox.classList.remove('loadScreen');
+        document.querySelector('.menuBtn').style.display = 'none';
+        document.querySelector('.menuAdd').style.display = 'none';
+    } else {
+        // mobileBox.classList.add('homeScreen');
+        mobileBox.classList.remove('phoneInner');
+        // document.getElementById('page').style.display = 'block';
+        On.disabled = true;
+        page = setInterval(() => {
+            // document.getElementById('page').style.display = 'none';
+            document.querySelector('.time').style.display = 'block';
+            document.querySelector('.monthTime').style.display = 'block';
+            mobileBox.classList.add('loadScreen');
+            document.querySelector('.menuBtn').style.display = 'block';
+            On.disabled = false;
+            clearTimeout(page);
+        },);
+    }
+}
+
+for (let index = 0; index < toggles.length; index++) {
+    toggles[index].addEventListener('click', function () {
+        console.log("Toggle clicked");
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = 'none';
+            saveToggleState(index, 'none');
+        } else {
+            panel.style.display = 'block';
+            saveToggleState(index, 'block');
+        }
+        updatePageState();
+    });
+}
+
+function currentTime() {
+    const now = new Date();
+    const fullDate = String(now).split(' ');
+    const [today, month, date] = fullDate;
+    const hrs = String(now.getHours()).padStart(2, 0);
+    const min = String(now.getMinutes()).padStart(2, 0);
+    time.innerHTML = `<h1>${hrs}:${min}</h1>`;
+    monthTime.innerHTML = `<h5>${today}, ${month} ${String(date).padStart(2, 0)}</h5>`;
+}
+
+window.onload = function () {
+    updatePageState();
+    currentTime();
+};
+
+setInterval(currentTime, 1000);
+
+menuBtn.addEventListener('click', function () {
+    document.querySelector('.menuAdd').style.display = 'block';
+    document.querySelector('.menuBtn').style.display = 'none';
+    document.querySelector(".otherMenu").style.display='block'
+
+});
+
+phone.addEventListener('click', function () {
+    window.location.href = './UU';
+});
+gallary.addEventListener('click',function(){
+    mobileBox.classList.add('cameraImage');
+    document.querySelector('.time').style.display='none'
+    document.querySelector('.monthTime').style.display='none'
+    document.querySelector('.menuAdd').style.display = 'none';
+    document.querySelector(".otherMenu").style.display='none'
+    document.getElementById('snap').style.display='block'
+ 
+})
+gallary.addEventListener('click',function(){
+    document.querySelector('.home').style.display='block'
+
+ 
+})
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const snapButton = document.getElementById('snap');
+const startCameraButton = document.getElementById('startCamera');
+const snapshots = []; 
+function offCamera(){
+    navigator.mediaDevices.getUserMedia({ torch: false })
+    .then(function (stream) {
+        video.srcObject = stream;
+        video.play();
+    })
+    .catch(function (err) {
+        console.error('Error accessing the camera: ', err);
+    });
+
+}
+// offCamera()
+
+function startCamera() {
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function (stream) {
+            video.srcObject = stream;
+            video.play();
+        })
+        .catch(function (err) {
+            console.error('Error accessing the camera: ', err);
+        });
+}
+camera.addEventListener('click',function(){
+    mobileBox.classList.add('cameraImage');
+    document.querySelector('.time').style.display='none'
+    document.querySelector('.monthTime').style.display='none'
+    document.querySelector('.menuAdd').style.display = 'none';
+    document.querySelector(".otherMenu").style.display='none'
+})
+camera.addEventListener('click',function(){
+    startCamera();
+      video.style.display = 'block'; 
+    document.getElementById('snap').style.display='block'
+    document.querySelector('.home').style.display='block'
+})
+
+
+snapButton.addEventListener('click', function () {
+    const context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    // const dataUrl= canvas.toDataURL('image/png');
+
+    snapshots.push(dataUrl);
+});
+snapshots.forEach(function(el){
+
+})
+home.addEventListener('click',function(){
+    mobileBox.classList.remove('cameraImage');
+    document.querySelector('.time').style.display='block'
+    document.querySelector('.monthTime').style.display='block'
+    document.querySelector('.menuAdd').style.display = 'block';
+    document.querySelector(".otherMenu").style.display='block'
+})
+home.addEventListener('click',function(){
+    video.style.display = 'none'; 
+    document.querySelector('.home').style.display='none'
+    document.getElementById('snap').style.display='none'
+
+
+ 
+})
+home.addEventListener('click',function(){
+ window.location.href="/index.html"
+
+})
